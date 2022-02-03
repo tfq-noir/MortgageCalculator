ab1edab66ab4f9ef5bb7cfd482789b6e2ee72162
Md Towfique hasan Bhuiyan <t.bhuiyan@sportradar.com>
2022-02-03 18:43:12 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
+    public static void main(String[] args) {
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+
+        MortgageReport.printMortgage(principal, annualInterest, years);
+        MortgageReport.printPaymentSchedule(principal, annualInterest, years);
+    }
+
+}
